dc3779c9b5f52edce9ddf39f627e690c3581d21f
Thomas <tyic2834@gmail.com>
2025-09-23 00:28:24 +0800

    fix router

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import DocEditView from "../views/DocEditView.vue";
 import LoginView from "../views/LoginView.vue";
@@ -19,7 +19,7 @@ function ifAuthenticated(to, from, next) {
   next({ name: 'login' })
 }
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
